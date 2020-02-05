a95caa2f1895ee6919883dff42842fac63c8b4d7
Allan Lin <allanlin97@gmail.com>
2020-02-05 15:29:43 -0500

    #4: added login activity and client fragment

---
+++ app/src/main/java/com/allanlin97gmail/sanal/LoginInActivity.java
@@ -0,0 +1,159 @@
+package com.allanlin97gmail.sanal;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class LoginInActivity extends AppCompatActivity {
+
+    GoogleSignInClient mGoogleSignInClient;
+    int RC_SIGN_IN = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_in);
+
+        findViewById(R.id.sign_in_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("test", "hi");
+                switch (view.getId()) {
+                    case R.id.sign_in_button:
+                        signIn();
+                        break;
+
+                }
+            }
+        });
+
+        // Configure sign-in to request the user's ID, email address, and basic
+        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .requestIdToken(getString(R.string.server_client_id))
+                .build();
+
+        // Build a GoogleSignInClient with the options specified by gso.
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        GoogleSignInClient googleSignInClient = GoogleSignIn.getClient (this, gso);
+
+        googleSignInClient.silentSignIn ()
+                .addOnCompleteListener (this, new OnCompleteListener<GoogleSignInAccount>() {
+
+                    @Override
+                    public void onComplete (@NonNull Task<GoogleSignInAccount> task) {
+                        handleSignInResult(task);
+                    }
+                });
+
+    }
+
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+            String idToken = account.getIdToken();
+
+            // TODO(developer): send ID Token to server and validate
+//            HttpClient httpClient = new DefaultHttpClient();
+//            HttpPost httpPost = new HttpPost("https://yourbackend.example.com/tokensignin");
+//
+//            try {
+//                List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(1);
+//                nameValuePairs.add(new BasicNameValuePair("idToken", idToken));
+//                httpPost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+//
+//                HttpResponse response = httpClient.execute(httpPost);
+//                int statusCode = response.getStatusLine().getStatusCode();
+//                final String responseBody = EntityUtils.toString(response.getEntity());
+//                Log.i("SignIn", "Signed in as: " + responseBody);
+//            } catch (ClientProtocolException e) {
+//                Log.e("idback", "Error sending ID token to backend.", e);
+//            } catch (IOException e) {
+//                Log.e("errorid", "Error sending ID token to backend.", e);
+//            }
+
+
+            // Signed in successfully, show authenticated UI.
+            //updateUI(account);
+            Intent intent = new Intent(LoginInActivity.this, MainActivity.class);
+            startActivity(intent);
+        } catch (ApiException e) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.w("TAG", "signInResult:failed code=" + e.getStatusCode());
+            //updateUI(null);
+        }
+    }
+
+    //public void onStart() {
+
+    //super.onStart();
+
+    // Check for existing Google Sign In account, if the user is already signed in
+// the GoogleSignInAccount will be non-null.
+    //GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+    //updateUI(account);
+    //}
+
+//    public void  updateUI(GoogleSignInAccount account){
+//        if(account != null){
+//            Toast.makeText(this,"U Signed In successfully",Toast.LENGTH_LONG).show();
+//            startActivity(new Intent(this,MainActivity.class));
+//        }else {
+//            Toast.makeText(this,"U Didnt signed in",Toast.LENGTH_LONG).show();
+//        }
+//    }
+
+}
+
+
