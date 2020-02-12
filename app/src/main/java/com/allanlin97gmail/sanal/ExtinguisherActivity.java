3da4ee933d63ce3d0cd0f10f80194b83838586df
Allan Lin <allanlin97@gmail.com>
2020-02-12 15:20:10 -0500

    #6: added camera intent and functionatility

--- app/src/main/java/com/allanlin97gmail/sanal/ExtinguisherActivity.java
+++ app/src/main/java/com/allanlin97gmail/sanal/ExtinguisherActivity.java
@@ -1,13 +1,21 @@
 package com.allanlin97gmail.sanal;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.app.DatePickerDialog;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -18,6 +26,9 @@ public class ExtinguisherActivity extends AppCompatActivity {
 
     final Calendar myCalendar = Calendar.getInstance();
     EditText editText;
+    ImageView imageView;
+    Button photoButton;
+    public  static final int RequestPermissionCode  = 1 ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +36,21 @@ public class ExtinguisherActivity extends AppCompatActivity {
         setContentView(R.layout.activity_extinguisher);
 
         editText = findViewById(R.id.extinguisherMDate);
+        imageView = findViewById(R.id.imageView1);
+        photoButton = findViewById(R.id.button1);
+
+        EnableRuntimePermission();
+
+        photoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+
+                startActivityForResult(intent, 7);
+
+            }
+        });
 
 
         final DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener(){
@@ -49,18 +75,58 @@ public class ExtinguisherActivity extends AppCompatActivity {
                 return true;
             }
         });
-
-
-
     }
 
     private void updateLabel() {
         String myFormat = "MM/dd/yyyy"; //In which you need put here
         SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
-
-        // edittext.setText(sdf.format(myCalendar.getTime()));
         editText.setText(sdf.format(myCalendar.getTime()));
     }
 
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (requestCode == 7 && resultCode == RESULT_OK) {
+
+            Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+
+            imageView.setImageBitmap(bitmap);
+        }
+    }
+
+    public void EnableRuntimePermission(){
+
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                Manifest.permission.CAMERA))
+        {
+
+            Toast.makeText(this,"CAMERA permission allows us to Access CAMERA app", Toast.LENGTH_LONG).show();
+
+        } else {
+
+            ActivityCompat.requestPermissions(this,new String[]{
+                    Manifest.permission.CAMERA}, RequestPermissionCode);
+
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int RC, String per[], int[] PResult) {
+
+        switch (RC) {
+
+            case RequestPermissionCode:
+
+                if (PResult.length > 0 && PResult[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    Toast.makeText(this,"Permission Granted, Now your application can access CAMERA.", Toast.LENGTH_LONG).show();
+
+                } else {
+
+                    Toast.makeText(this,"Permission Canceled, Now your application cannot access CAMERA.", Toast.LENGTH_LONG).show();
+
+                }
+                break;
+        }
+    }
 
 }
