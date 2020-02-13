c5c2021779e2aba3bed792b2fd6d3900c222e0d8
Allan Lin <allanlin97@gmail.com>
2020-02-13 14:51:40 -0500

    #4: add alert box to client screen for adding clients to the client list

--- app/src/main/java/com/allanlin97gmail/sanal/MainActivity.java
+++ app/src/main/java/com/allanlin97gmail/sanal/MainActivity.java
@@ -5,13 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.ArrayList;
 
@@ -20,14 +24,27 @@ public class MainActivity extends AppCompatActivity {
     RecyclerView recyclerView;
     RecyclerView.Adapter adapter;
     RecyclerView.LayoutManager layoutManager;
+    FloatingActionButton addClientFab;
+    ArrayList<ClientItem> clientList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        addClientFab = findViewById(R.id.addClientFab);
+
+        addClientFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                dialogBox();
+                //showDiag();
+            }
+        });
+
         // ArrayList for client info
-        ArrayList<ClientItem> clientList = new ArrayList<>();
+        clientList = new ArrayList<>();
         clientList.add(new ClientItem("Bob", "bob@gmail.com","5199838383","4514 Dougall"));
         clientList.add(new ClientItem("2", "2","2","2"));
         clientList.add(new ClientItem("as", "asd","asd","sda"));
@@ -61,6 +78,49 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    public void dialogBox() {
+
+        final View dialogView = View.inflate(this,R.layout.add_client,null);
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+        alertDialogBuilder.setMessage("Add Client");
+
+        // set up the edit text
+        final EditText name = (EditText) dialogView.findViewById(R.id.addName);
+        final EditText address = (EditText) dialogView.findViewById(R.id.addAddress);
+        final EditText phone = (EditText) dialogView.findViewById(R.id.addPhone);
+        final EditText email = (EditText) dialogView.findViewById(R.id.addEmail);
+
+        alertDialogBuilder.setPositiveButton("Create",
+                new DialogInterface.OnClickListener() {
+
+                    @Override
+                    public void onClick(DialogInterface arg0, int arg1) {
+                        name.getText().toString().trim().length();
+                        address.getText().toString().trim().length();
+                        phone.getText().toString().trim().length();
+                        email.getText().toString().trim().length();
+
+                        ClientItem clientItem = new ClientItem(name.getText().toString(), address.getText().toString(), phone.getText().toString(), email.getText().toString());
+                        clientList.add(clientItem);
+                    }
+                });
+
+        alertDialogBuilder.setNegativeButton("Cancel",
+                new DialogInterface.OnClickListener() {
+
+                    @Override
+                    public void onClick(DialogInterface arg0, int arg1) {
+
+                    }
+                });
+
+
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        alertDialog.setView(dialogView);
+        alertDialog.show();
+    }
+
 }
 
 
